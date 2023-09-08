6a1658c477104b5f0eda760686cf6a6fd36fad3a
Ajay <112794893+Ajay-308@users.noreply.github.com>
2023-09-08 19:57:28 +0530

    working on toggle

--- static/javascript/javascript.js
+++ static/javascript/javascript.js
@@ -16,3 +16,23 @@ function closeMenu() {
   hamburger.classList.remove("active");
   navMenu.classList.remove("active");
 }
+
+const toggleButton = document.getElementById("toggleButton");
+const body = document.body;
+const image = document.getElementById("image");
+let isWhitesmoke = true;
+
+toggleButton.addEventListener("click", function () {
+  if (body.classList.contains("whitesmoke-bg")) {
+    body.classList.remove("whitesmoke-bg");
+    body.classList.add("black-bg");
+    console.log("white");
+    image.src = "../static/image/black.png"; // Change to the black-themed image
+  } else {
+    body.classList.remove("black-bg");
+    body.classList.add("whitesmoke-bg");
+    console.log("black");
+    image.src = "../static/image/jump_girl.jpeg"; // Change back to the default image
+  }
+  isWhitesmoke = !isWhitesmoke;
+});
